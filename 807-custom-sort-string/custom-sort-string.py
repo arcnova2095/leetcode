88fe938f68b754bc4be0ebbb5f01ef9ea6156730
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-03-11 10:31:27 +0530

    Time: 39 ms (28.74%) | Memory: 16.5 MB (91.57%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,17 +1,15 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        char_count = {char: 0 for char in order}
-        for char in s:
-            if char in char_count:
-                char_count[char] += 1
-    
-        sorted_s = ''
-        for char in order:
-            sorted_s += char * char_count[char]
-    
-        for char in s:
-            if char not in order:
-                sorted_s += char
-
-        return sorted_s
-        
+        count = [0]*26
+        for i in s:
+            count[ord(i)-ord('a')] += 1
+        n = ''
+        for i in order:
+            while count[ord(i)-ord('a')] > 0:
+                n += i
+                count[ord(i)-ord('a')] -= 1
+        for i in range(26):
+            while count[i] > 0:
+                n += chr(i + ord('a'))
+                count[i] -= 1
+        return n
