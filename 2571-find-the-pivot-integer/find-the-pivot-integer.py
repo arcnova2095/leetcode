3e121cd5582265bcddfddf9109e007a70506781b
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-03-14 00:37:54 +0530

    Time: 31 ms (96.24%) | Memory: 16.5 MB (59.17%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,8 +1,17 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        x= sqrt(n*(n+1)/2)
-        if x%1!=0:
-            return -1
-        else:
-            return int(x)
+        if n == 1:
+            return 1
+        l = 0
+        r = n
+        s = n * (n+1)//2
+        while l < r:
+            m = l + (r-l)//2
+            if m * m < s:
+                l = m + 1
+            elif m * m > s:
+                r = m - 1
+            else:
+                return m
+        return -1
         
