8a09a16a1bbf590ad661fa1eb83ee075589b7a84
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-04-10 10:03:01 +0530

    Time: 333 ms (6.16%) | Memory: 24.3 MB (23.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,26 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        island=0
-        visited= set()
-        rows = len(grid)
+        row= len(grid)
         col= len(grid[0])
-        def bfs(r,c):
-            q= deque()
-            visited.add((r,c))
-            q.append((r,c))
-            while q:
-                row,cols=q.popleft()
-                directions=[[1,0],[-1,0],[0,1],[0,-1]]
-                for dr, dc in directions:
-                    r,c= row+dr,cols+dc
-                    if r in range(rows) and c in range(col) and grid[r][c]=='1' and (r,c) not in visited:
-                        q.append((r,c))
-                        visited.add((r,c))
-        for r in range(rows):
+        res=0
+        path=set()
+        def dfs(r,c):
+            if ( r not in range(row) or c not in range(col) or grid[r][c]=="0" or (r,c) in path):
+                return 
+            path.add((r,c))
+            directions=[[0,1],[1,0],[-1,0],[0,-1]]
+            for rw,cl in directions:
+                dfs(r+rw,c+cl)
+        for r in range(row):
             for c in range(col):
-                if grid[r][c]=="1" and (r,c) not in visited:
-                    bfs(r,c)
-                    island+=1
-        return island
-        
+                if ((r,c) not in path and grid[r][c]=='1'):
+                    res+=1
+                    dfs(r,c)
+        return res
+            
+            
+            
