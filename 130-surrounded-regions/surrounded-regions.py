6a29772f33a79f0118ecdfc3a508105d8bea1ce1
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-04-11 10:59:06 +0530

    Time: 114 ms (85.99%) | Memory: 17.9 MB (55.77%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,36 +1,24 @@
-from typing import List
-
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        if not board:
-            return
-        
-        row, col = len(board), len(board[0])
-        
-        
-        def dfs(r, c):
-            if 0 <= r < row and 0 <= c < col and board[r][c] == 'O':
-                board[r][c] = 'T'  
-                
-                dfs(r + 1, c)
-                dfs(r - 1, c)
-                dfs(r, c + 1)
-                dfs(r, c - 1)
-        
-       
-        for r in range(row):
-            for c in [0, col - 1]: 
-                if board[r][c] == 'O':
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        Rows,Cols= len(board),len(board[0])
+        def dfs(r,c):
+            if r not in range(Rows) or c not in range(Cols) or board[r][c]!="O":
+                return 
+            board[r][c]="T"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for r in range(Rows):
+            for c in range(Cols):
+                if board[r][c] == "O" and (r in [0, Rows - 1] or c in [0, Cols - 1]):
                     dfs(r, c)
-        for c in range(col):
-            for r in [0, row - 1]:  
-                if board[r][c] == 'O':
-                    dfs(r, c)
-        
-        
-        for r in range(row):
-            for c in range(col):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'  
-                elif board[r][c] == 'T':
-                    board[r][c] = 'O' 
+        for r in range(Rows):
+            for c in range(Cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c]=="T":
+                    board[r][c] ="O"
