35fa1443222079ac8ba9253f2623ecb8081df0a5
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-04-18 00:11:33 +0530

    Time: 32 ms (74.32%) | Memory: 16.6 MB (31.79%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,16 +1,13 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        t0=0
-        t1=1
-        t2=1
-    
-        while(n>0):
-            t=t1+t0+t2
-            t0=t1
-            t1=t2
-            t2= t
-            n-=1
-        return t0
+        def solve(n,t0,t1,t2):
+            if n==0:
+                return t0
+            else:
+                n-=1
+                return solve(n,t1,t2,t0+t1+t2)
+        return solve(n,0,1,1)
+
     
         
         
