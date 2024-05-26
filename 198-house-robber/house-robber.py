e647da5ccc052038b4c60f6083b9bb03fbd74e20
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-05-26 13:13:14 +0530

    Time: 32 ms (79.87%) | Memory: 16.5 MB (89.10%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         a,b=0,0
-        for i in nums:
+        for i in (nums):
             temp= max(a+i,b)
             a=b
             b= temp
