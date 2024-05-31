3fadc54d8b7f42279e67f99de42b06516b622e67
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-05-31 22:39:36 +0530

    Time: 341 ms (5.05%) | Memory: 24.2 MB (29.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,23 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        row= len(grid)
-        col= len(grid[0])
         res=0
-        path=set()
+        visited=set()
+        row,col= len(grid),len(grid[0])
+
         def dfs(r,c):
-            if ( r not in range(row) or c not in range(col) or grid[r][c]=="0" or (r,c) in path):
-                return 
-            path.add((r,c))
-            directions=[[0,1],[1,0],[-1,0],[0,-1]]
-            for rw,cl in directions:
-                dfs(r+rw,c+cl)
+            
+            if r not in range(row) or c not in range(col) or (r,c)  in visited or grid[r][c]=="0":
+                return 0
+            visited.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
         for r in range(row):
             for c in range(col):
-                if ((r,c) not in path and grid[r][c]=='1'):
-                    res+=1
-                    dfs(r,c)
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    res += 1
+                    dfs(r, c)
         return res
-            
-            
-            
+
