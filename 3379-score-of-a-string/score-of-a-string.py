10d586fbd51a4b8a1afdc63eb7b291c2868b0687
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-06-01 11:25:34 +0530

    Time: 42 ms (19.60%) | Memory: 16.4 MB (82.79%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -5,8 +5,10 @@ class Solution:
         for i in s:
             arr.append(ord(i))
         for i in range(len(arr)-1):
-            res+=abs(arr[i]-arr[i+1])
-            
+            if max(arr[i],arr[i+1])== arr[i+1]:
+                res+= arr[i+1]-arr[i]
+            else:
+                res+=arr[i]-arr[i+1]
         return res
             
         
