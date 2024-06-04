c6af750b73ddf8dc00b75423c54729c3e4e8408a
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-06-04 19:23:19 +0530

    Time: 55 ms (27.61%) | Memory: 16.7 MB (23.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,28 +1,26 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh,time=0,0
-        ROW,COL= len(grid), len(grid[0])
-        q= deque()
-        for i in range(ROW):
-            for j in range(COL):
-                if grid[i][j]== 2:
-                    q.append([i,j])
-                elif grid[i][j]==1:
+        fresh=0
+        time=0
+        row,col=len(grid),len(grid[0])
+        q=deque()
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c]==1:
                     fresh+=1
-        
-        while q and fresh>0:
-            direct=[[0,1],[0,-1],[1,0],[-1,0]]
+                if grid[r][c]==2:
+                    q.append([r,c])
+        d= [[0,1],[1,0],[-1,0],[0,-1]]
+
+        while fresh>0 and q:
             for i in range(len(q)):
                 r,c= q.popleft()
-                for rc,cc in direct:
-                    row,col= r+rc, c+cc
-                    if row not in range(ROW) or col not in range(COL) or grid[row][col]!=1:
-                        continue
-                    grid[row][col]=2
-                    
-                    q.append([row,col])
-                    fresh-=1
+                for dr,dc in ((d)):
+                    rd,cd= r+dr,dc+c 
+                    if rd in range(row) and cd in range(col) and grid[rd][cd]==1:
+                        grid[rd][cd]=2
+                        q.append([rd,cd])
+                        fresh-=1
             time+=1
-        
+
         return time if fresh==0 else -1
-                
