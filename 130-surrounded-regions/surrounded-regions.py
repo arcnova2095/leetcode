f3c1db2b872bf0724833a8260091ff49ae79dc5f
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-06-08 10:19:02 +0530

    Time: 125 ms (32.99%) | Memory: 17.8 MB (95.71%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -5,12 +5,12 @@ class Solution:
         """
         Rows,Cols= len(board),len(board[0])
         def dfs(r,c):
-            if r not in range(Rows) or c not in range(Cols) or board[r][c]!="O":
-                return 
+            if r not in range (Rows) or c not in range(Cols) or board[r][c]!="O":
+                return
             board[r][c]="T"
             dfs(r+1,c)
-            dfs(r-1,c)
             dfs(r,c+1)
+            dfs(r-1,c)
             dfs(r,c-1)
         for r in range(Rows):
             for c in range(Cols):
@@ -18,7 +18,13 @@ class Solution:
                     dfs(r, c)
         for r in range(Rows):
             for c in range(Cols):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c]=="T":
-                    board[r][c] ="O"
+                if board[r][c]=="O":
+                    board[r][c]="X"
+                
+        for r in range(Rows):
+            for c in range(Cols):
+                if board[r][c]=="T":
+                    board[r][c]="O"
+
+
+                    
