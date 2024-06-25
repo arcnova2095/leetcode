7e7ab636ffd5014e074c52bf77ec75d52f0ca20d
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-06-25 11:07:19 +0530

    Time: 99 ms (65.71%) | Memory: 20.1 MB (26.76%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,19 +1,17 @@
 class Solution:
     def minDistance(self, w1: str, w2: str) -> int:
-        dp={}
-        def dfs(i,j):
-            if i==len(w1):
-                return len(w2)-j
-        
-            if j==len(w2):
-                return len(w1)-i
-            if (i,j) in dp:
-                return dp[(i,j)]
-            
-            if w1[i]== w2[j]:
-                dp[(i,j)]=dfs(i+1,j+1)
-            else:
-                dp[(i,j)]= 1+min(dfs(i+1,j),dfs(i,j+1),dfs(i+1,j+1))
-            return dp[(i,j)]
-        return dfs(0,0)
+        dp= [[0]* (len(w1)+1) for j in range(len(w2)+1)]
+        for i in range(len(w1) + 1):
+            dp[len(w2)][i] = len(w1) - i
+        for i in range(len(w2) + 1):
+            dp[i][len(w1)] = len(w2) - i
+        for i in range(len(w2)-1,-1,-1):
+            for j in range(len(w1)-1,-1,-1):
+                if w2[i]==w1[j]:
+                    dp[i][j]=dp[i+1][j+1]
+                else:
+                    dp[i][j]= 1+ min(dp[i+1][j+1],dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
+
+
             
