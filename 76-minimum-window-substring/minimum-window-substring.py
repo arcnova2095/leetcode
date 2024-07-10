00613d53509982bf4cffbce8cbefc6900c4d9d1c
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-07-10 16:43:35 +0530

    Time: 110 ms (67.58%) | Memory: 17.3 MB (57.64%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,31 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        if len(s) < len(t):
             return ""
+        
+        window, T = {}, {}
+        
+        for i in t:
+            T[i] = T.get(i, 0) + 1
+        have, need = 0, len(T)
 
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
+        res = [-1, -1]
+        resLen = float("infinity")
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
-
-            if c in countT and window[c] == countT[c]:
+            window[s[r]] = 1 + window.get(s[r], 0)
+            if s[r] in T and window[s[r]] == T[s[r]]:
                 have += 1
 
             while have == need:
-                
-                if (r - l + 1) < resLen:
+                if r - l + 1 < resLen:
                     res = [l, r]
                     resLen = r - l + 1
-          
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in T and window[s[l]] < T[s[l]]:
                     have -= 1
                 l += 1
+
         l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+        return s[l:r + 1] if resLen != float("infinity") else ""
