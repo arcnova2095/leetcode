3664ffbd7a993c5cc85411e345b6f5e5898104dd
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-07-19 08:58:40 +0530

    Time: 1115 ms (41.34%) | Memory: 32.4 MB (55.24%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -9,7 +9,7 @@ class Solution:
             q.append(r)
             while l>q[0]:
                 q.popleft()
-            if r+1>=k:
+            if r-l+1>=k:
                 output.append(nums[q[0]])
                 l+=1
             r+=1
