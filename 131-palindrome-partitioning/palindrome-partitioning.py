20ec3ae2b3f2c6946bff3d0888d85dfbf217c269
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-07-29 20:24:39 +0530

    Time: 445 ms (93.96%) | Memory: 35.3 MB (27.46%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,23 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def pal(s,l,r):
-            while (l<r):
-                if s[l]!=s[r]:
+        res=[]
+        sub=[]
+        def pal(l,r):
+            while l<=r:
+                if s[l]!= s[r]:
                     return False
                 l+=1
                 r-=1
             return True
-        res=[]
-        sub=[]
         def dfs(i):
             if i>= len(s):
-                res.append(sub[::])
+                res.append(sub.copy())
                 return
-            for j in range(i, len(s)):
-                if pal(s,i,j):
+            for j in range(i,len(s)):
+                if pal(i,j):
                     sub.append(s[i:j+1])
                     dfs(j+1)
                     sub.pop()
         dfs(0)
         return res
-            
+        
