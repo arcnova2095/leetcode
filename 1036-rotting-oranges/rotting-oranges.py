196b248b99e271e130f5c8758db0065527b88892
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-08-07 20:50:20 +0530

    Time: 58 ms (16.06%) | Memory: 16.5 MB (49.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,26 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh=0
-        time=0
-        row,col=len(grid),len(grid[0])
-        q=deque()
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c]==1:
+        fresh,time=0,0
+        q= deque()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
                     fresh+=1
-                if grid[r][c]==2:
-                    q.append([r,c])
-        d= [[0,1],[1,0],[-1,0],[0,-1]]
-
-        while fresh>0 and q:
+                if grid[i][j]==2:
+                    q.append((i,j))
+        d=[[0,1],[1,0],[-1,0],[0,-1]]
+        while q and fresh:
             for i in range(len(q)):
                 r,c= q.popleft()
-                for dr,dc in ((d)):
-                    rd,cd= r+dr,dc+c 
-                    if rd in range(row) and cd in range(col) and grid[rd][cd]==1:
-                        grid[rd][cd]=2
-                        q.append([rd,cd])
+                for dr,dc in d:
+                    row,col= r+dr,c+dc
+                    if row in range(len(grid)) and col in range(len(grid[0])) and grid[row][col]==1:
+                        grid[row][col]=2
+                        q.append((row,col))
                         fresh-=1
             time+=1
+        return time if fresh==0 else -1  
 
-        return time if fresh==0 else -1
+                
+
+    
