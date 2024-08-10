f0fea86d33cb0906a126c8d9c7b546c2120d0a54
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-08-10 15:02:19 +0530

    Time: 27 ms (96.48%) | Memory: 16.5 MB (63.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,12 +1,10 @@
 class Solution:
-    def helper(self,n):
-        a,b=0,0
-        for i in n:
-            rob=max(a+i,b)
-            a=b
-            b= rob
-        return b
-
     def rob(self, nums: List[int]) -> int:
-        return max(self.helper(nums[1:]), self.helper(nums[:-1]),nums[0])
-        
+        def help(nums):
+            a,b=0,0
+            for i in nums:
+                temp= max(b,a+i)
+                a=b
+                b= temp
+            return b
+        return max(help(nums[1:]), help(nums[:-1]), nums[0])
