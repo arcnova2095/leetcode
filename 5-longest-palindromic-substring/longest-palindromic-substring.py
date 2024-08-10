3472dbdcfb4f6d85dd568a575c1bd65f54e8d345
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-08-10 15:12:33 +0530

    Time: 330 ms (63.59%) | Memory: 16.6 MB (38.40%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -3,22 +3,23 @@ class Solution:
         res=""
         length=0
         for i in range(len(s)):
-            l,r=i,i
-            while l>=0 and r <len(s) and s[l]==s[r]:
-                if r-l+1>= length:
+            l=i
+            r=i
+            while (l>=0 and r<len(s) and s[l]==s[r]):
+                if length<=r-l+1:
+                    length= r-l+1
                     res= s[l:r+1]
-                    length=r-l+1
-                r+=1
                 l-=1
-            l,r=i,i+1
-            while l>=0 and r <len(s) and s[l]==s[r]:
-                if r-l+1>= length:
-                    res= s[l:r+1]
-                    length=r-l+1
                 r+=1
-                l-=1
-        return res
-             
             
-
+            l=i
+            r=i+1
+            while (l>=0 and r<len(s) and s[l]==s[r]):
+                if length<=r-l+1:
+                    length= r-l+1
+                    res= s[l:r+1]
+                l-=1
+                r+=1
+        return res
+            
                 
