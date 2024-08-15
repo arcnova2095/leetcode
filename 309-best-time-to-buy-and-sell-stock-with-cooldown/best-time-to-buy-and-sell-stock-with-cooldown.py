0379dbd1193b388e47134d3f41a339424880de57
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-08-15 10:19:29 +0530

    Time: 59 ms (10.65%) | Memory: 18 MB (21.50%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,13 +7,14 @@ class Solution:
                 return 0
             if (i,buying) in dp:
                 return dp[(i,buying)]
+            
             cooldown= dfs(i+1,buying)
-
             if buying:
-                buy= dfs(i+1,not buying)-prices[i]
+                buy= dfs(i+1, not buying)-prices[i]
                 dp[(i,buying)]= max(cooldown,buy)
             else:
-                sell= dfs(i+2,not buying)+prices[i]
+                sell= dfs(i+2, not buying)+ prices[i]
                 dp[(i,buying)]= max(cooldown,sell)
             return dp[(i,buying)]
        return dfs(0,True)
+
