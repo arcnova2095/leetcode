9b11626fe9dff3b696813158612a6487b927342a
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-09-05 19:00:44 +0530

    Time: 1110 ms (9.09%) | Memory: 27.4 MB (32.89%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        s=mean*(n+len(rolls))
+        
+        s-= sum(rolls)
+        arr=[]
+        while(n):
+            x=(math.floor(s/n))
+            if x>6 or x<1:
+                return []
+            arr.append(x)
+            s-=x
+            n-=1
+        if (s):
+            arr[1]+=1
+        return arr
