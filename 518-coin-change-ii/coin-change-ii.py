9f90787cbe6fa697f4f17cea98076fbe06d9787d
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-09-12 10:21:21 +0530

    Time: 166 ms (63.11%) | Memory: 16.9 MB (64.28%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp=[0]*(amount+1)
+        row= [0]* (amount+1)
+        row[0]=1
         for i in range(len(coins)-1,-1,-1):
             newRow= [0]*(amount+1)
             newRow[0]=1
-            for j in range(1,amount+1):
-                newRow[j]= dp[j]
-                if j-coins[i]>=0:
-                    newRow[j]+= newRow[j-coins[i]]
-            dp= newRow
-        return dp[amount]
+            for a in range(1,amount+1):
+                newRow[a]= row[a]
+                if (a-coins[i])>=0:
+                    newRow[a]+=newRow[a-coins[i]]
+            row= newRow
+        return row[amount]
+
 
             
 
