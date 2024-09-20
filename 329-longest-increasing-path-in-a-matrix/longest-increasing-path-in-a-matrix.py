94fb8b5d938168c4f55a57ea96df23ed932fd659
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-09-20 08:49:59 +0530

    Time: 422 ms (13.25%) | Memory: 21.2 MB (28.57%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,16 +1,19 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         dp={}
-        def dfs(i,j,preval):
-            if i not in range(len(matrix)) or j not in range(len(matrix[0])) or matrix[i][j]<=preval:
+
+        def dfs(r,c,prev):
+            if r not in range(len(matrix)) or c not in range(len(matrix[0])) or matrix[r][c]<= prev:
                 return 0
-            if (i,j) in dp:
-                return dp[(i,j)]
+            if (r,c) in dp:
+                return dp[(r,c)]
             res=1
-            res= max(res,1+ dfs(i+1,j,matrix[i][j]),1+ dfs(i-1,j,matrix[i][j]),1+ dfs(i,j-1,matrix[i][j]),1+ dfs(i,j+1,matrix[i][j]))
-            dp[(i,j)]=res
+            res=max(res,1+dfs(r+1,c,matrix[r][c]), 1+dfs(r-1,c,matrix[r][c]),1+dfs(r,c+1,matrix[r][c]),1+dfs(r,c-1, matrix[r][c])) 
+            dp[(r,c)]= res
             return res
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 dfs(i,j,-1)
         return max(dp.values())
+
+        
