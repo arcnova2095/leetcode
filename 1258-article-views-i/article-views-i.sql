dfb94ed67e3c04a619d61657e6a522d184d3b362
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-09-23 23:32:51 +0530

    Time: 441 ms (37.53%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select distinct viewer_id as id from Views where author_id=viewer_id order by id ASC;
+# Write your MySQL query statement belows
+select distinct author_id as id from Views where author_id=viewer_id order by author_id ASC;
