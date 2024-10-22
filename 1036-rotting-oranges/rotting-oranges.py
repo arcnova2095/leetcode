ec669852a99b6dd29dc743bb42e4e02a5dde4499
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-10-22 10:22:23 +0530

    Time: 3 ms (99.33%) | Memory: 16.8 MB (7.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh,time=0,0
         q= deque()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
+        fresh=0
+        time =0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c]==1:
                     fresh+=1
-                if grid[i][j]==2:
-                    q.append((i,j))
-        d=[[0,1],[1,0],[-1,0],[0,-1]]
-        while q and fresh:
-            for i in range(len(q)):
-                r,c= q.popleft()
-                for dr,dc in d:
-                    row,col= r+dr,c+dc
-                    if row in range(len(grid)) and col in range(len(grid[0])) and grid[row][col]==1:
-                        grid[row][col]=2
-                        q.append((row,col))
-                        fresh-=1
-            time+=1
-        return time if fresh==0 else -1  
+                if grid[r][c]==2:
+                    q.append((r,c))
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while fresh > 0 and q:
+            length = len(q)
+            for i in range(length):
+                r, c = q.popleft()
 
-                
-
-    
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    # if in bounds and nonrotten, make rotten
+                    # and add to q
+                    if (
+                        row in range(len(grid))
+                        and col in range(len(grid[0]))
+                        and grid[row][col] == 1
+                    ):
+                        grid[row][col] = 2
+                        q.append((row, col))
+                        fresh -= 1
+            time += 1
+        return time if fresh==0 else -1
