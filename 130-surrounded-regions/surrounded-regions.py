780a970dfaeba08a7b060dbd3d14261eae7a6545
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-10-22 10:51:01 +0530

    Time: 11 ms (97.34%) | Memory: 20.7 MB (22.23%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,28 +3,28 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        Rows,Cols= len(board),len(board[0])
-        def dfs(r,c):
-            if r not in range (Rows) or c not in range(Cols) or board[r][c]!="O":
-                return
-            board[r][c]="T"
-            dfs(r+1,c)
-            dfs(r,c+1)
-            dfs(r-1,c)
-            dfs(r,c-1)
-        for r in range(Rows):
-            for c in range(Cols):
-                if board[r][c] == "O" and (r in [0, Rows - 1] or c in [0, Cols - 1]):
-                    dfs(r, c)
-        for r in range(Rows):
-            for c in range(Cols):
+        row,col= len(board), len(board[0])
+        def dfs(i,j):
+            if i not in range(row) or j not in range(col) or board[i][j]!="O":
+                return 
+            board[i][j]="T"
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+            dfs(i-1,j)
+            
+
+        for r in range(row):
+            for c in range(col):
+                if board[r][c]=="O" and r in [0,row-1] or c in [0,col-1]:
+                    dfs(r,c)
+        for r in range(row):
+            for c in range(col):       
                 if board[r][c]=="O":
                     board[r][c]="X"
-                
-        for r in range(Rows):
-            for c in range(Cols):
+        for r in range(row):
+            for c in range(col):
                 if board[r][c]=="T":
                     board[r][c]="O"
-
-
-                    
+                
+        
