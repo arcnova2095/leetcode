c4ba665cf81ce334471922450473e31387d50485
arcnova2095 <91647204+arcnova2095@users.noreply.github.com>
2024-10-23 11:59:35 +0530

    Time: 3 ms (99.31%) | Memory: 18.3 MB (65.78%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,23 @@
 class Solution:
-    def canFinish(self, n: int, p: List[List[int]]) -> bool:
-        hm= {i: [] for i in range(n)}
-
-        for crs, pre in p:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hm= {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
             hm[crs].append(pre)
-        visit= set()
+        path= set()
 
-        def dfs(crs):
-            if crs in visit:
+        def dfs(i):
+            if i in path:
                 return False
-            if hm[crs]==[]:
+            if hm[i]==[]:
                 return True
-            visit.add(crs)
-
-            for pre in hm[crs]:
+            path.add(i) 
+            for pre in hm[i]:
                 if not dfs(pre):
                     return False
-            visit.remove(crs)
-            hm[crs]=[]
+            path.remove(i)
+            hm[i]=[]
             return True
-
-        for i in range(n):
-            if not dfs(i):
+        for c in range(numCourses):
+            if not dfs(c):
                 return False
         return True
-
-
